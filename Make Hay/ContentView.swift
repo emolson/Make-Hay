5701a694e014985fa55220b30f4f6e1f996cc235
emolson <emolson@mtu.edu>
2025-12-31 07:27:45 -0600

    When sun shines changes

--- Make Hay/ContentView.swift
+++ Make Hay/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "Make Hay!"
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Make Hay!")
+            Text(message)
+            
+            Button("Change Text") {
+                message = "When Sun Shines!"
+            }
+            .padding(.top)
         }
         .padding()
     }
