bba0ffb35359feabfe0792cd07100520634637cf
emolson <emolson@mtu.edu>
2025-12-31 06:38:54 -0600

    Initial Commit

---
+++ Make Hay/Make_HayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Make_HayApp.swift
+//  Make Hay
+//
+//  Created by Ethan Olson on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct Make_HayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Make HayTests/Make_HayTests.swift
@@ -0,0 +1,17 @@
+//
+//  Make_HayTests.swift
+//  Make HayTests
+//
+//  Created by Ethan Olson on 12/31/25.
+//
+
+import Testing
+@testable import Make_Hay
+
+struct Make_HayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
